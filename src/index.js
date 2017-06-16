c2e8e2d977560ab34371f2631f03d27ffcf1bedb
Wajiha Kanwal <wajihakanwal012@gmail.com>
2017-06-16 09:49:56 +0500

    Declare winner

--- src/index.js
+++ src/index.js
@@ -32,6 +32,9 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -40,7 +43,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -81,48 +90,26 @@ class Game extends React.Component {
     }
 }
 
-// class ShoppingList extends React.Component{
-//     render() {
-//         return (
-//             React.createElement(
-//                 'div',
-//                 {className: 'MyShoppingList'},
-//                 React.createElement(
-//                     'h1',
-//                     null,
-//                     'My Shopping List'
-//                 ),
-//                 React.createElement(
-//                     'ul',
-//                     null,
-//                     React.createElement(
-//                         'li',
-//                         null,
-//                         'Potatoes'
-//                     ),
-//                     React.createElement(
-//                         'li',
-//                         null,
-//                         'Cabbage'
-//                     ),
-//                     React.createElement(
-//                         'li',
-//                         null,
-//                         'Flour'
-//                     )
-//                 )
-//             )
-//         )
-//     }
-// }
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
 
-// ========================================
-
-
-// ReactDOM.render(
-//     <ShoppingList />,
-//     document.getElementById('root')
-// );
 ReactDOM.render(
     <Game />,
     document.getElementById('root')
